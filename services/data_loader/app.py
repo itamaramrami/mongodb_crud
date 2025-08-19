de410de9af765d9ef588cab2da545e3f08a2909b
itamar <ya0534516378@gmail.com>
2025-08-19 17:30:51 +0300

    app

--- services/data_loader/app.py
+++ services/data_loader/app.py
@@ -1,1 +1,19 @@
-print("hello yakir")
+from fastapi import FastAPI
+from .DAL import *
+import os
+
+app = FastAPI()
+
+MONGO_USER = os.getenv("MONGO_USER", "root")
+MONGO_PASS = os.getenv("MONGO_PASS", "root123")
+MONGO_HOST = os.getenv("MONGO_HOST", "mongo-service")
+MONGO_PORT = os.getenv("MONGO_PORT", "27017")
+
+uri = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/?authSource=admin"
+
+loader = DataLoader(uri)
+loader.init_data()
+
+@app.get("/data")
+def get_data():
+    return loader.get_all()
---
+++ services/data_loader/solider.py
@@ -0,0 +1,8 @@
+class Solider:
+    def __init__(self ,id:int,first_name:str,last_name:str,phone_number:int,rank:str):
+        self.id=id
+        self.first_name=first_name
+        self.last_name=last_name
+        self.phone_number=phone_number
+        self.rank=rank
+    
