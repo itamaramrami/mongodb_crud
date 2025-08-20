c5a7c49af9f403b7813e5c1d56e18b4f0a48a560
itamar <ya0534516378@gmail.com>
2025-08-20 16:12:17 +0300

    finish

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -2,3 +2,40 @@
 @REM הרצת קונטינר mongodb
 docker run --name mongodb -p 27017:27017 -d mongodb/mongodb-community-server:latest
 
+
+
+@REM בניית Img ודחיפה docker hub
+docker build -t itamar12/mongodb-crud-server:latest .
+docker push itamar12/mongodb-crud-server:latest
+
+
+@REM מחיקת תוכן הפרוייקט
+oc delete all --all
+oc delete pvc --all
+
+
+
+בניית פוד עם servis וpvc
+ oc apply -f infrastructure/k8s/pvc.yaml
+ oc apply -f infrastructure/k8s/deployment_mongo.yaml
+ oc apply -f infrastructure/k8s/service_mongo.yaml
+
+
+oc apply -f infrastructure/k8s/deployment_fastapi.yaml
+oc apply -f infrastructure/k8s/service_fastapi.yaml
+oc apply -f infrastructure/k8s/route.yaml 
+
+
+
+@REM update
+https://fastapi-route-ya0534516378-dev.apps.rm1.0a51.p1.openshiftapps.com/update/?id=8&field=first_name&value=kkk
+
+@REM insert
+https://fastapi-route-ya0534516378-dev.apps.rm1.0a51.p1.openshiftapps.com/insert/?id=8&first_name=Itamar&last_name=Levi&phone_number=0501234567&rank=Private
+
+@REM get
+https://fastapi-route-ya0534516378-dev.apps.rm1.0a51.p1.openshiftapps.com/data
+
+
+@REM delete
+https://fastapi-route-ya0534516378-dev.apps.rm1.0a51.p1.openshiftapps.com/delete/?id=9
--- services/data_loader/app.py
+++ services/data_loader/app.py
@@ -1,20 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 from DAL import DataLoader
 from solider import Solider
-import os
 
 app = FastAPI()
 
 
-uri = "mongodb://localhost:27017"
+uri = "mongodb://mongodb:27017"
+loader=DataLoader(uri)
 
-# loader = DataLoader(uri)
-# solid=Solider(2,"it","kk",12,"ll")
-# loader.insert(solid)
-# loader.delete(1)
-# loader.get_all()
-# loader.update(1,"rank","ooo")
+
+@app.get("/")
+def home():
+    return {"message": "hello"}
 
 @app.get("/data")
 def get_data():
     return loader.get_all()
+
+
+@app.get("/insert/")
+def add_soldier(
+    id = Query(...),
+    first_name = Query(...),
+    last_name = Query(...),
+    phone_number = Query(...),
+    rank = Query(...)
+):
+    soldier = Solider(id, first_name, last_name, phone_number, rank)
+    loader.insert(soldier)
+    return {"message": "yyyyy"}
+
+@app.get("/delete/")
+def delete_data(
+    id = Query(...)
+    ):
+    
+    return loader.delete(id)
+
+
+
+@app.get("/update/")
+def update_data(
+    id = Query(...),
+    field=Query(...),
+    value=Query(...)
+    ):
+    return loader.update(id,field,value)
+
+
